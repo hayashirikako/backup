ebad56dac874f3cca79c1948682ed8166f6babc9
hayashirikako <r1-kyatannnn6262@ezweb.ne.jp>
2017-12-22 21:47:01 +0900

    1222

---
+++ script.js
@@ -0,0 +1,148 @@
+
+document.write(3*3);
+document.write(4/2);
+
+var a=5+10;
+document.write(a);
+
+var b=55-28;
+document.write(b);
+
+var c=10*2;
+document.write(c);
+
+var d="3+6";
+document.write(d);
+
+var e="2+3";
+var f="4-1";
+document.write(e);
+document.write("<br>");
+document.write(f);
+
+var g=5*5;
+var h="5*5";
+document.write(h+"="+g);
+
+var abc1 =10%3;
+document.write(abc1);
+
+var abc2=5%2;
+document.write("5÷2の余りは"+abc2);
+
+var abc3="8%2";
+document.write(abc3);
+
+var abc4=3%2;
+var abc5=1;
+document.write(abc4+"="+abc5);
+
+var a1=3;
+document.write(++a1);
+
+var a2=20;
+document.write(++a2);
+
+var a3=10;
+document.write(++a3);
+document.write("<br>");
+document.write(++a3);
+
+var a4=5;
+document.write(a4++);
+document.write("<br>");
+document.write(a4++);
+document.write("<br>");
+document.write(a4++);
+
+var a5=15;
+document.write(--a5);
+document.write("<br>");
+document.write(--a5);
+
+var a6=20;
+document.write(a6--);
+document.write("<br>");
+document.write(a6--);
+document.write("<br>");
+document.write(a6--);
+
+var a7=2;
+document.write(a7--);
+document.write(a7--);
+
+var a8=2;
+document.write(++a8);
+document.write(a8++);
+document.write("<br>");
+document.write(a8);
+
+var a9=1;
+document.write(a9++);
+document.write("<br>");
+document.write(a9++);
+document.write("<br>");
+document.write(a9);
+
+var a1=2;
+if(a1<10){document.write("A");}
+
+var a2=10;
+if(a2<=10){
+    document.write("B");}
+
+var a4=12;
+if(a4==12){
+    document.write("同じ数字");
+}
+
+
+var a5="田中";
+if(a5=="田中"){
+    document.write(a5+"さんです");
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
